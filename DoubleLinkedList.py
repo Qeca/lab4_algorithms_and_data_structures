d2ba4e93a82f97f30c4a64b450beed7e0fc0fd23
4219 Sharonov D.V <dima.sharonov.2004@mail.ru>
2023-10-30 14:21:18 +0300

    Реализована структура данных двухсвязный список, написаны гномья сортировка и сортировка подсчётом для экземпляров класса Book, написаны тесты для всех методов

---
+++ DoubleLinkedList.py
@@ -0,0 +1,249 @@
+import random
+from dataclasses import dataclass
+from typing import TypeVar, Generic, Optional, Callable
+
+T = TypeVar("T")
+
+
+class IndexOutRangeException(Exception):
+    pass
+
+
+@dataclass
+class Book:
+    def __init__(self, author, publisher, page_count, price, isbn):
+        self.author = author
+        self.publisher = publisher
+        self.page_count = page_count
+        self.price = price
+        self.isbn = isbn
+
+    def __str__(self):
+        return f"({self.author}, {self.publisher}, {self.page_count}, {self.price}, {self.isbn})"
+
+
+@dataclass
+class DoublyNode(Generic[T]):
+    data: T
+    next_ptr: Optional['DoublyNode[T]'] = None
+    prev_ptr: Optional['DoublyNode[T]'] = None
+
+
+class DoublyLinkedList(Generic[T]):
+
+    def __init__(self) -> None:
+        self._length: int = 0
+        self._head: Optional[DoublyNode[T]] = None
+        self._tail: Optional[DoublyNode[T]] = None
+
+    def __len__(self) -> int:
+        return self._length
+
+    def _check_range(self, index: int) -> bool:
+        if index >= self._length or index < 0:
+            return False
+        return True
+
+    def append(self, data: T) -> None:
+        node = DoublyNode[T](data, None)
+        if self._length <= 0:
+            self._head = node
+            self._tail = node
+            self._length += 1
+            return
+
+        self._tail.next_ptr = node
+        node.prev_ptr = self._tail
+        self._tail = node
+        self._length += 1
+
+    def push_head(self, data: T) -> None:
+        node = DoublyNode[T](data)
+        if self._length <= 0:
+            self._head = node
+            self._tail = node
+            self._length += 1
+            return
+
+        node.next_ptr = self._head
+        self._head.prev_ptr = node
+        self._head = node
+        self._length += 1
+
+    def insert(self, index: int, data: T) -> None:
+        ok: bool = self._check_range(index)
+        if not ok:
+            raise IndexOutRangeException("-_-")
+
+        if index == 0:
+            self.push_head(data)
+            return
+        elif index == self._length - 1:
+            self.append(data)
+            return
+
+        node = self._head
+        for i in range(0, index):
+            node = node.next_ptr
+
+        insert_node = DoublyNode[T](data)
+        insert_node.next_ptr = node
+        node.prev_ptr.next_ptr = insert_node
+        insert_node.prev_ptr = node.prev_ptr
+        node.prev_ptr = insert_node
+        self._length += 1
+
+    def get(self, index: int) -> T:
+        ok: bool = self._check_range(index)
+        if not ok:
+            raise IndexOutRangeException("-_-")
+
+        if index == 0:
+            return self._head.data
+        if index == self._length - 1:
+            return self._tail.data
+
+        node = self._head
+        for i in range(0, index):
+            node = node.next_ptr
+        return node.data
+
+    def set(self, index: int, data: T) -> T:
+        ok: bool = self._check_range(index)
+        if not ok:
+            raise IndexOutRangeException("-_-")
+        node = self._head
+        for i in range(0, index):
+            node = node.next_ptr
+        node.data = data
+
+    def remove(self, index: int) -> bool:
+        ok: bool = self._check_range(index)
+        if not ok:
+            return False
+
+        if index == 0:
+            node = self._head
+            self._head = node.next_ptr
+            self._head.prev_ptr = None
+            del node
+            self._length -= 1
+            return True
+
+        node = self._head
+        for i in range(0, index - 1):
+            node = node.next_ptr
+
+        if index == self._length - 1:
+            self._tail.prev_ptr = None
+            self._tail = node
+            self._tail.next_ptr = None
+            self._length -= 1
+            return True
+
+        delete_node = node.next_ptr
+        node.next_ptr = delete_node.next_ptr
+        node.next_ptr.prev_ptr = delete_node.prev_ptr
+        self._length -= 1
+        return True
+
+    def __str__(self) -> str:
+        my_str: str = ""
+        node = self._head
+        while node is not None:
+            my_str += str(node.data) + " "
+            node = node.next_ptr
+        return f"[{my_str}]"
+
+    def find_max(self, key: Optional[Callable[[T], bool]] = None) -> Optional[T]:
+        if self._length == 0:
+            return None
+
+        max_value = self._head.data
+        node = self._head.next_ptr
+        while node is not None:
+            if key is not None and key(node.data):
+                if key(node.data) > key(max_value):
+                    max_value = node.data
+            else:
+                if node.data > max_value:
+                    max_value = node.data
+            node = node.next_ptr
+        return max_value
+
+    def find_min(self, key: Optional[Callable[[T], bool]] = None) -> Optional[T]:
+        if self._length == 0:
+            return None
+
+        min_value = self._head.data
+        node = self._head.next_ptr
+        while node is not None:
+            if key is not None and key(node.data):
+                if key(node.data) < key(min_value):
+                    min_value = node.data
+            else:
+                if node.data < min_value:
+                    min_value = node.data
+            node = node.next_ptr
+        return min_value
+
+    def gnome_sort(self):
+        index = 1
+        i = 0
+        n = self._length
+        while i < n - 1:
+            if self.get(i).author <= self.get(i + 1).author:
+                i, index = index, index + 1
+            else:
+                a_i, a_i_1 = self.get(i + 1), self.get(i)
+                self.set(i, a_i)
+                self.set(i + 1, a_i_1)
+                i = i - 1
+                if i < 0:
+                    i, index = index, index + 1
+
+    def counting_sort(self):
+        min_value = self.find_min(key=lambda x: x.page_count).page_count
+        max_value = self.find_max(key=lambda x: x.page_count).page_count
+        support = [0 for i in range(max_value - min_value + 1)]
+        sup_1 = [self.get(i).page_count for i in range(len(self))]
+        for el in range(len(self)):
+            support[self.get(el).page_count - min_value] += 1
+        index = len(self) - 1
+        sup_2 = []
+        for i in range(len(support)):
+            for el in range(support[i]):
+                sup_2.append(i + min_value)
+                index -= 1
+        a = []
+        for k in range(len(sup_2)):
+            a.append(self.get(sup_1.index(sup_2[k])))
+            sup_1[sup_1.index(sup_2[k])] = None
+        for ind in range(len(self)):
+            self.set(ind,a[ind])
+
+
+
+
+
+if __name__ == '__main__':
+    llist = DoublyLinkedList()
+    books = [
+        Book("Jane Austen", "Vintage Classics", 368, 14, "978-0099589273"),
+        Book("George Orwell", "Signet Classic", 328, 9, "978-0451524935"),
+        Book("J.K. Rowling", "Bloomsbury Publishing", 332, 19, "978-0747558194"),
+        Book("Harper Lee", "Harper Perennial Modern Classics", 376, 8, "978-0061120084"),
+        Book("F. Scott Fitzgerald", "Scribner", 180, 7, "978-0743273565"),
+        Book("William Golding", "Penguin Books", 224, 9, "978-0143129400"),
+        Book("J.R.R. Tolkien", "Mariner Books", 1178, 14, "978-0544003415"),
+        Book("George R.R. Martin", "Bantam", 694, 9, "978-0553593716"),
+        Book("Mark Twain", "Dover Publications", 224, 3, "978-0486400778"),
+        Book("Leo Tolstoy", "Vintage Classics", 1392, 12, "978-0670021049"),
+        Book("Agatha Christie", "William Morrow Paperbacks", 288, 7, "978-0062073485"),
+        Book("Ernest Hemingway", "Scribner", 332, 10, "978-0684801469")
+    ]
+    print(len(books))
+    for book in books:
+        llist.append(book)
+    llist.counting_sort()
+    print(llist)
---
+++ doublelinkedlist_test.py
@@ -0,0 +1,117 @@
+import unittest
+from DoubleLinkedList import DoublyLinkedList, Book
+
+
+class TestLinkedList(unittest.TestCase):
+
+    def test_append_and_str_int(self):
+        llist = DoublyLinkedList()
+        for i in range(1, 11):
+            llist.append(i)
+        self.assertEqual(f'{llist}', '[1 2 3 4 5 6 7 8 9 10 ]')
+
+    def test_len(self):
+        llist = DoublyLinkedList()
+        for i in range(11):
+            llist.append(i)
+        llist1 = DoublyLinkedList()
+        for i in range(16):
+            llist1.append(i)
+        llist2 = DoublyLinkedList()
+        for i in range(25):
+            llist2.append(i)
+        self.assertEqual(len(llist), 11)
+        self.assertEqual(len(llist1), 16)
+        self.assertEqual(len(llist2), 25)
+
+    def test_remove(self):
+        llist = DoublyLinkedList()
+        for i in range(11):
+            llist.append(i)
+        llist.remove(3)
+        self.assertEqual(f'{llist}', '[0 1 2 4 5 6 7 8 9 10 ]')
+
+    def test_insert(self):
+        llist = DoublyLinkedList()
+        for i in range(11):
+            llist.append(i)
+        llist.insert(3, 22)
+        self.assertEqual(f"{llist}", "[0 1 2 22 3 4 5 6 7 8 9 10 ]")
+
+    def test_get(self):
+        llist = DoublyLinkedList()
+        for i in range(11):
+            llist.append(i)
+        self.assertEqual(llist.get(5), 5)
+
+    def test_head(self):
+        llist = DoublyLinkedList()
+        for i in range(11):
+            llist.append(i)
+        llist.push_head(12)
+        self.assertEqual(f"{llist}", "[12 0 1 2 3 4 5 6 7 8 9 10 ]")
+
+    def test_set(self):
+        llist = DoublyLinkedList()
+        for i in range(11):
+            llist.append(i)
+        llist.set(0, 100)
+        self.assertEqual(f'{llist}', "[100 1 2 3 4 5 6 7 8 9 10 ]")
+
+    def test_find_max(self):
+        llist = DoublyLinkedList()
+        tuples = [(1, 2), (3, 4), (5, -1), (6, 146)]
+        for i in tuples:
+            llist.append(i)
+        self.assertEqual(llist.find_max(key=lambda x: x[1]), (6, 146))
+
+    def test_find_min(self):
+        llist = DoublyLinkedList()
+        tuples = [(1, 2), (3, 4), (5, -1), (6, 146)]
+        for i in tuples:
+            llist.append(i)
+        self.assertEqual(llist.find_min(key=lambda x: x[0]), (1, 2))
+
+    def test_gnome_sort(self):
+        llist = DoublyLinkedList()
+        books = [
+            Book("Jane Austen", "Vintage Classics", 368, 14, "978-0099589273"),
+            Book("George Orwell", "Signet Classic", 328, 9, "978-0451524935"),
+            Book("J.K. Rowling", "Bloomsbury Publishing", 332, 19, "978-0747558194"),
+            Book("Harper Lee", "Harper Perennial Modern Classics", 376, 8, "978-0061120084"),
+            Book("F. Scott Fitzgerald", "Scribner", 180, 7, "978-0743273565"),
+            Book("William Golding", "Penguin Books", 224, 9, "978-0143129400"),
+            Book("J.R.R. Tolkien", "Mariner Books", 1178, 14, "978-0544003415"),
+            Book("George R.R. Martin", "Bantam", 694, 9, "978-0553593716"),
+            Book("Mark Twain", "Dover Publications", 224, 3, "978-0486400778"),
+            Book("Leo Tolstoy", "Vintage Classics", 1392, 12, "978-0670021049"),
+            Book("Agatha Christie", "William Morrow Paperbacks", 288, 7, "978-0062073485"),
+            Book("Ernest Hemingway", "Scribner", 332, 10, "978-0684801469")
+        ]
+        for i in books:
+            llist.append(i)
+        llist.gnome_sort()
+        self.assertEqual(f'{llist}', "[(Agatha Christie, William Morrow Paperbacks, 288, 7, 978-0062073485) (Ernest Hemingway, Scribner, 332, 10, 978-0684801469) (F. Scott Fitzgerald, Scribner, 180, 7, 978-0743273565) (George Orwell, Signet Classic, 328, 9, 978-0451524935) (George R.R. Martin, Bantam, 694, 9, 978-0553593716) (Harper Lee, Harper Perennial Modern Classics, 376, 8, 978-0061120084) (J.K. Rowling, Bloomsbury Publishing, 332, 19, 978-0747558194) (J.R.R. Tolkien, Mariner Books, 1178, 14, 978-0544003415) (Jane Austen, Vintage Classics, 368, 14, 978-0099589273) (Leo Tolstoy, Vintage Classics, 1392, 12, 978-0670021049) (Mark Twain, Dover Publications, 224, 3, 978-0486400778) (William Golding, Penguin Books, 224, 9, 978-0143129400) ]")
+
+    def test_counting_sort(self):
+        llist = DoublyLinkedList()
+        books = [
+            Book("Jane Austen", "Vintage Classics", 368, 14, "978-0099589273"),
+            Book("George Orwell", "Signet Classic", 328, 9, "978-0451524935"),
+            Book("J.K. Rowling", "Bloomsbury Publishing", 332, 19, "978-0747558194"),
+            Book("Harper Lee", "Harper Perennial Modern Classics", 376, 8, "978-0061120084"),
+            Book("F. Scott Fitzgerald", "Scribner", 180, 7, "978-0743273565"),
+            Book("William Golding", "Penguin Books", 224, 9, "978-0143129400"),
+            Book("J.R.R. Tolkien", "Mariner Books", 1178, 14, "978-0544003415"),
+            Book("George R.R. Martin", "Bantam", 694, 9, "978-0553593716"),
+            Book("Mark Twain", "Dover Publications", 224, 3, "978-0486400778"),
+            Book("Leo Tolstoy", "Vintage Classics", 1392, 12, "978-0670021049"),
+            Book("Agatha Christie", "William Morrow Paperbacks", 288, 7, "978-0062073485"),
+            Book("Ernest Hemingway", "Scribner", 332, 10, "978-0684801469")
+        ]
+        for i in books:
+            llist.append(i)
+        llist.counting_sort()
+        self.assertEqual(f"{llist}", "[(F. Scott Fitzgerald, Scribner, 180, 7, 978-0743273565) (William Golding, Penguin Books, 224, 9, 978-0143129400) (Mark Twain, Dover Publications, 224, 3, 978-0486400778) (Agatha Christie, William Morrow Paperbacks, 288, 7, 978-0062073485) (George Orwell, Signet Classic, 328, 9, 978-0451524935) (J.K. Rowling, Bloomsbury Publishing, 332, 19, 978-0747558194) (Ernest Hemingway, Scribner, 332, 10, 978-0684801469) (Jane Austen, Vintage Classics, 368, 14, 978-0099589273) (Harper Lee, Harper Perennial Modern Classics, 376, 8, 978-0061120084) (George R.R. Martin, Bantam, 694, 9, 978-0553593716) (J.R.R. Tolkien, Mariner Books, 1178, 14, 978-0544003415) (Leo Tolstoy, Vintage Classics, 1392, 12, 978-0670021049) ]")
+if __name__ == '__main__':
+    unittest.main()
